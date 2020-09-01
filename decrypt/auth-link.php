f1f2f641a0343859c06178f32ef5487a046bb12e
Adam Clark <adam.clark@activehq.co.uk>
2020-09-01 15:17:27 +0100

    Update auth-link.php

--- decrypt/auth-link.php
+++ decrypt/auth-link.php
@@ -9,24 +9,34 @@ require_once "disciple/decrypt.php";
 # This is the Secret Key that Disciple provide to you. It is NOT your API key.
 $secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 
+# Now we hex2bin it
 $key = hex2bin($secret_key);
 
+# Get the variable "_dm" passed inthe URL
 $input = $_GET['_dm'];
+
+# Call the function (in the file '/disciple/decrypt.php')
 $JSN = decrypt_and_verify($input, $key);
 
 
-/* RENDER IT OUT - OR DO SOMETHING WITH IT */
+
+# It's done! Simples!
+
+
+/* NOW RENDER IT OUT - OR DO SOMETHING WITH IT */
 echo "<h3>JSN=</h3>";
 echo "<pre>";
 print_r($JSN);
 echo "</pre>";
 
 $decodedJSN = json_decode($JSN);
-
 echo "<h3>DECODED JSN=</h3>";
 echo "<pre>";
 print_r($decodedJSN);
 echo "</pre>";
+
+
+
 
 
 /* OR DO SOMETHING WITH IT */
